ed0a2beb36e1ea1ec1be40ae9ec3f8746821e28c
salmahayaa <salmaksm06@gmail.com>
2022-03-20 23:43:36 +0700

    tugas pertama

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller {
+
+	public function index()
+	{
+		echo "<h1> perkenalkan </h1>";
+        echo "nama saya Salmahaya Dian Putri Kusuma
+            saya tingal didaerah depok
+            olahraga yang saya sukai adalah
+            bulutangkis";
+	}
+}
